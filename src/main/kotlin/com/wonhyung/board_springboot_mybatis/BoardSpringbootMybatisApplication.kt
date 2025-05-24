f91a23826b6512d2bb25eba29e0b4ee877475775
1hyung <ryu1hyung@naver.com>
2025-05-24 23:49:07 +0900

    :sparkles: feat: Initial commit

---
+++ src/main/kotlin/com/wonhyung/board_springboot_mybatis/BoardSpringbootMybatisApplication.kt
@@ -0,0 +1,11 @@
+package com.wonhyung.board_springboot_mybatis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoardSpringbootMybatisApplication
+
+fun main(args: Array<String>) {
+	runApplication<BoardSpringbootMybatisApplication>(*args)
+}
---
+++ src/test/kotlin/com/wonhyung/board_springboot_mybatis/BoardSpringbootMybatisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wonhyung.board_springboot_mybatis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoardSpringbootMybatisApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
